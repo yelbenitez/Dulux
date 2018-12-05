d1dcbccf612085f205abb14f047f856d64872d28
Yel <yelbenitez5@gmail.com>
2018-12-05 13:43:30 -0800

    happy path

--- functions/index.js
+++ functions/index.js
@@ -12,25 +12,47 @@ const {
 } = require('actions-on-google');
 
 admin.initializeApp();
-//firebase = admin.database();
-//const app = dialogflow();
-/*
-app.middleware((conv)=>{
-
-});*/
 
 exports.fulfillment = functions.https.onRequest((request,response)=>{
     const agent = new WebhookClient({request,response});
-
+    const bodyParser = request.body.queryResult;
+    const db = admin.database();
+  
     //Backend functions ///////////////////////////////////////////
 
-    function readData(project,agent){
-        return admin.database().ref( project + '/validation').once('value').then(function(snapshot){// working get
-            agent.add(JSON.stringify(snapshot.val()));
+    function getOptions(project,agent){
+        return admin.database().ref( project + '/validation/0').once('value').then(function(snapshot){// working get
+            agent.add("Nice, a " + project + ". Is your " + project + " " + snapshot.val().option_1 + " or " +snapshot.val().option_2 + " ?"); // good
         });
     }
 
-    // Intent ////////////////////////////////////////////////////
+    function getChoiceValidation(choice,agent){ //testing
+        const ch1_selected = bodyParser.outputContexts[0].parameters["Option_1.original"];
+        const ch2_selected = bodyParser.outputContexts[0].parameters["Option_2.original"];
+        const db = admin.database();
+
+        if(ch1_selected != ""){
+            return db.ref('validations/choice_1/' + ch1_selected).once('value').then(function(snapshot){// good
+                    return confirmationRespond(snapshot.val(),agent);
+            });
+         }else if(ch2_selected != ""){
+            return db.ref('validations/choice_2/' + ch2_selected).once('value').then(function(snapshot){// good
+                     return confirmationRespond(snapshot.val(),agent);
+            });
+         }
+    }
+
+    function confirmationRespond(respond,agent){
+        const project_type = bodyParser.outputContexts[1].parameters["Project.original"]; // getting the context from JSON request of dflow 
+ 
+        return db.ref('val_response/' + respond).once('value').then(function(snapshot){// good
+        const message = snapshot.val().first_line+" "+project_type+" "+snapshot.val().second_line+" "+project_type+" to dry. "+snapshot.val().third_line
+        
+        agent.add(message+" Are you ready?");
+        });
+    }
+
+    // Intents ////////////////////////////////////////////////////
 
     function Welcome(agent) {
         const initMessage = `Welcome to the Dulux Genie.
@@ -43,76 +65,34 @@ exports.fulfillment = functions.https.onRequest((request,response)=>{
     }
 
     function Start_project(agent) {
-        const param_project = agent.parameters.Project;
-         const param_descrption = agent.parameters.Description
-        
-        const project_type = request.body.queryResult.outputContexts[1].parameters["Project.original"]; // getting the context from JSON request of dflow 
-        return readData(project_type,agent)
-      //  agent.add("Nice, a " + project_type + ". Is your " + project_type + " Indoor or outdoor?"); 
-        
-        /*
-        if(param_description == ""){
-          //      const context = agent.setContext('Project_type');
-                agent.add("Nice a " + param_project);
-       //      return Validation(agent);
-         }else{
-             agent.add("oops");
-         }*/
-
+        const project_type = bodyParser.outputContexts[1].parameters["Project.original"]; // getting the context from JSON request of dflow 
+        return getOptions(project_type,agent)
     }
 
     function Validation(agent) {
-     //   const context = agent.getContext('Project_type');
-     //   agent.add();
-    }
+        const choice_1 = bodyParser.parameters["Option_1"]; //Good
+        const choice_2 = bodyParser.parameters["Option_2"];
 
+        if(choice_1 != ""){ //Good
+            return getChoiceValidation(choice_1,agent);
+        }else if(choice_2 != ""){
+            return getChoiceValidation(choice_2,agent);
+        }else{
+            agent.add("I think we are not on the same page ?");
+        }
+    }   
+
+    function Confirmation(agent) {
+     /*   return db.ref('validations/choice_2/' + ch2_selected).once('value').then(function(snapshot){// good
+            return confirmationRespond(snapshot.val(),agent);
+         });*/
+    }
 
     let intentMap = new Map();
     intentMap.set('Default Welcome Intent', Welcome);
     intentMap.set('Find Project', Start_project);
-    intentMap.set('Validation', Validation)
+    intentMap.set('Validation', Validation);
+    intentMap.set('Confirmation', Confirmation);
 
     agent.handleRequest(intentMap);
 });
-
-
-/*
-app.intent('Default Welcome Intent', (conv)=> {
-       // conv.contexts.set(Context.PROJECT,5);
-        const initMessage = `Welcome to the Dulux Genie.
-        I’m here to help you take on your 
-        project, although you’ll have to 
-        take on the painting yourself.
-        To start off, what project do you 
-        need help with`;
-   //     conv.ask(initMessage);
-        return readData(userId,conv);
-});
-
-
-app.intent('Find Project', (conv)=>{
-        return readRequest(conv);
-});
-*/
-
-function writeUserData(userId, name, email) {   //data insert working
-    admin.database().ref('users/' + userId).set({
-      username: name,
-      email: email
-    });
-}
-
-
-function readData(userId,conv){
-    return admin.database().ref('users/' + userId + '/email').once('value').then(function(snapshot){// working get
-         //   conv.ask(snapshot.val());
-         return messagePost(snapshot.val(),conv);
-    });
-}
-
-    function messagePost(message,conv){// working get
-        return conv.ask('reached '+message);
-    }
-
-
-//exports.fulfillment = functions.https.onRequest(app); // firebase cloud functions
