c438729057fdfdcaf65d8d3e5ee90a3762858bc0
Yel <yelbenitez5@gmail.com>
2018-12-05 09:56:12 -0800

    working context parsing

--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,8 @@
 const functions = require('firebase-functions');
 const {dialogflow} = require('actions-on-google');
 const Datastore = require('@google-cloud/datastore');
+const {WebhookClient} = require('dialogflow-fulfillment');
+const admin = require('firebase-admin');
 const {
   SimpleResponse,
   BasicCard,
@@ -9,51 +11,60 @@ const {
   Button
 } = require('actions-on-google');
 
-const admin = require('firebase-admin');
-
 admin.initializeApp();
 //firebase = admin.database();
-
-const app = dialogflow({debug: true});
-
+//const app = dialogflow();
+/*
 app.middleware((conv)=>{
 
-});
+});*/
 
-var userId = 100;
-var name = "yel";
-var email = "yelbenitez5@gmail.com"
+exports.fulfillment = functions.https.onRequest((request,response)=>{
+    const agent = new WebhookClient({request,response});
 
+    function Welcome(agent) {
+        const initMessage = `Welcome to the Dulux Genie.
+        I’m here to help you take on your 
+        project, although you’ll have to 
+        take on the painting yourself.
+        To start off, what project do you 
+        need help with13`;   
+        agent.add(initMessage);
+    }
 
+    function Start_project(agent) {
+        const param_project = agent.parameters.Project;
+         const param_descrption = agent.parameters.Description
+        
+        const project_type = request.body.queryResult.outputContexts[1].parameters["Project.original"]; // getting the context from JSON request of dflow 
+     
+        /*
+        if(param_description == ""){
+          //      const context = agent.setContext('Project_type');
+                agent.add("Nice a " + param_project);
+       //      return Validation(agent);
+         }else{
+             agent.add("oops");
+         }*/
 
-function writeUserData(userId, name, email) {   //data insert
-    admin.database().ref('users/' + userId).set({
-      username: name,
-      email: email
-    });
-}
+    }
 
-function readData(userId,conv){
-    
-    return admin.database().ref('users/' + userId + '/email').once('value').then(function(snapshot){// working get
-         //   conv.ask(snapshot.val());
-         return messagePost(snapshot.val(),conv);
-    });
-}
-
-    function messagePost(message,conv){// working get
-        return conv.ask('reached '+message);
+    function Validation(agent) {
+     //   const context = agent.getContext('Project_type');
+     //   agent.add();
     }
 
 
+    let intentMap = new Map();
+    intentMap.set('Default Welcome Intent', Welcome);
+    intentMap.set('Find Project', Start_project);
+    intentMap.set('Validation', Validation)
+
+    agent.handleRequest(intentMap);
+});
 
 
-function tryReturn(userId,conv){ // this is not working
-    var res = admin.database().ref('users/' + userId + '/email').once('value');
-    conv.ask(res.snapshot.val());
-}
-
-
+/*
 app.intent('Default Welcome Intent', (conv)=> {
        // conv.contexts.set(Context.PROJECT,5);
         const initMessage = `Welcome to the Dulux Genie.
@@ -66,45 +77,43 @@ app.intent('Default Welcome Intent', (conv)=> {
         return readData(userId,conv);
 });
 
-//Setup contexts
-
 
 app.intent('Find Project', (conv)=>{
-      //  conv.ask('Hello');
-      return tryReturn(userId,conv);
+        return readRequest(conv);
 });
-
-
-
-
-
-
-
-
-
-
-/*
-app.intent(WELCOME_INTENT, (conv)=>{
-    conv.ask("Welcome to Mr.Space facts generator! Ask me something about the great cosmos")
-})
-
-
-app.intent(FALLBACK_INTENT, (conv) =>{
-    conv.ask("Sorry I didn't understand your request")
-})
-
-
-app.intent(ABOUT_SPACE, (conv) =>{
-    const fact_type = conv.parameters[FACT_ENTITY].toLowerCase();
-    if(fact_type == "space"){
-        conv.ask("Space is made up of almost emptiness (Vacuum)")
-    } else if(fact_type == "planet" || fact_type == "planets"){
-        conv.ask("Our solar system has eight planets: Mercury, Venus, Earth, Mars, Jupiter, Saturn, Uranus and Neptune")        
-    } else if(fact_type == "star" || fact_type == "stars" ){
-        conv.ask("Every star you see in the night sky is bigger and brighter than our sun.")
-    }
-})
 */
 
-exports.fulfillment = functions.https.onRequest(app); // firebase cloud functions
+function readRequest(conv) {
+    request.post(
+        { json: { key: 'value' } },
+        function (error, response, body) {
+            if (!error && response.statusCode == 200) {
+                conv.ask(body);
+            }else{
+                conv.ask(body);
+            }
+        }
+    );
+}
 
+function writeUserData(userId, name, email) {   //data insert working
+    admin.database().ref('users/' + userId).set({
+      username: name,
+      email: email
+    });
+}
+
+
+function readData(userId,conv){
+    return admin.database().ref('users/' + userId + '/email').once('value').then(function(snapshot){// working get
+         //   conv.ask(snapshot.val());
+         return messagePost(snapshot.val(),conv);
+    });
+}
+
+    function messagePost(message,conv){// working get
+        return conv.ask('reached '+message);
+    }
+
+
+//exports.fulfillment = functions.https.onRequest(app); // firebase cloud functions
